027f2c6338c18e951c7560fcd4e0c19493de575c
S1lence-z <j3.zelenka@gmail.com>
2024-11-22 10:56:21 +0100

    feat: add single form data source

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <script setup>
+import { reactive } from 'vue';
 import EnergySection from './components/sections/EnergySection.vue';
 import HeatSection from './components/sections/HeatSection.vue';
 import HeatingSourcesSection from './components/sections/HeatingSourcesSection.vue';
 import VehicleSection from './components/sections/VehicleSection.vue';
 import EmissionsSection from './components/sections/EmissionsSection.vue';
 import CompanyInfoSection from './components/sections/CompanyInfoSection.vue';
+
+const formData = reactive({
+  energy: {},
+  heat: {},
+  heatingSources: {},
+  vehicles: {},
+  emissions: {},
+  companyInfo: {}
+});
+
+const handleSubmit = () => {
+	const rawFormData = JSON.parse(JSON.stringify(formData));
+	console.log('Submit: ', rawFormData);
+};
 </script>
 
 <template>
 	<div class="app">
 		<h1>Výpočet uhlíkové stopy</h1>
 		<div class="form-container">
-			<EnergySection />
+			<EnergySection v-model="formData.energy" />
 			<div class="section-divider"></div>
-			<HeatSection />
+			<HeatSection v-model="formData.heat" />
 			<div class="section-divider"></div>
-			<HeatingSourcesSection />
+			<HeatingSourcesSection v-model="formData.heatingSources" />
 			<div class="section-divider"></div>
-			<VehicleSection />
+			<VehicleSection v-model="formData.vehicles" />
 			<div class="section-divider"></div>
-			<EmissionsSection />
+			<EmissionsSection v-model="formData.emissions" />
 			<div class="section-divider"></div>
-			<CompanyInfoSection />
+			<CompanyInfoSection v-model="formData.companyInfo" />
+		</div>
+		<div>
+			<button @click="handleSubmit" class="submit-button">Submit</button>
 		</div>
 	</div>
 </template>
@@ -50,5 +68,21 @@ h1 {
 	margin: 2rem 0;
 	border-radius: 1px;
 	opacity: 0.7;
+}
+
+.submit-button {
+	display: block;
+	margin: 2rem auto;
+	padding: 1rem 2rem;
+	background-color: #007bff; /* Primary color */
+	color: #ffffff; /* Text on primary color */
+	border: none;
+	border-radius: 0.5rem;
+	cursor: pointer;
+	font-size: larger;
+}
+
+.submit-button:hover {
+	background-color: #0056b3; /* Primary color hover */
 }
 </style>
