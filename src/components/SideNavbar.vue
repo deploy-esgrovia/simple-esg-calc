2e14f40e3bab3e71c10f04063b32301d0053abad
S1lence-z <j3.zelenka@gmail.com>
2024-11-27 16:01:52 +0100

    chore: format side navbar

--- src/components/SideNavbar.vue
+++ src/components/SideNavbar.vue
@@ -1,47 +1,59 @@
 <template>
-	<div class="bg-gray-800 text-white">
-		<div>
-			<nav>
-				<ul>
-					<!--Scope 1-->
-					<li>
-						<ul>
-							<li class="text-lg text-black font-bold bg-indigo-500">Scope 1</li>
-							<li>
-								<router-link to="/" class="block py-2 text-black"
-									>Energy</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/heat" class="block py-2 text-black"
-									>Heat</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/heating-sources" class="block py-2 text-black"
-									>Heating Sources</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/vehicles" class="block py-2 text-black"
-									>Vehicles</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/emissions" class="block py-2 text-black"
-									>Emissions</router-link
-								>
-							</li>
-							<li>
-								<router-link to="/company-info" class="block py-2 text-black"
-									>Company Info</router-link
-								>
-							</li>
-						</ul>
-					</li>
-					<!--Scope 2 - další li element s ul elemente m uvnitř-->
-				</ul>
-			</nav>
-		</div>
+	<div>
+		<nav>
+			<ul>
+				<!--Scope 1-->
+				<li>
+					<ul>
+						<li class="text-lg font-bold">
+							<p>Scope 1</p>
+						</li>
+
+						<li>
+							<router-link to="/" class="block py-2 ">Energy</router-link>
+						</li>
+						<li>
+							<router-link to="/heat" class="block py-2 ">Heat</router-link>
+						</li>
+						<li>
+							<router-link to="/heating-sources" class="block py-2 "
+								>Heating Sources</router-link
+							>
+						</li>
+					</ul>
+				</li>
+				<!--Scope 2-->
+				<li>
+					<ul>
+						<li class="text-lg font-bold">Scope 2</li>
+						<li>
+							<router-link to="/vehicles" class="block py-2 ">Vehicles</router-link>
+						</li>
+						<li>
+							<router-link to="/emissions" class="block py-2 ">Emissions</router-link>
+						</li>
+					</ul>
+				</li>
+				<!--Scope 3-->
+				<li>
+					<ul>
+						<li class="text-lg font-bold">
+							<p>Scope 3</p>
+						</li>
+					</ul>
+				</li>
+				<!--Závěr-->
+				<li class="pt-4">
+					<ul>
+						<li class="text-lg  font-bold">Shrnutí</li>
+						<li>
+							<router-link to="/company-info" class="block py-2 "
+								>Company Info</router-link
+							>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
 	</div>
 </template>
