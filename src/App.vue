ea0720a79acdef90e5100dfa0a62cb7645f73db1
S1lence-z <j3.zelenka@gmail.com>
2024-12-04 15:33:23 +0100

    fix: fix sidebar import in app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import SideNavbar from './components/SideNavBar.vue';
+import SideNavbar from './components/SideNavbar.vue';
 </script>
 
 <template>
-	<div class="mt-4 bg-white border border-gray-200 rounded-xl shadow-sm grid grid-cols-[auto,1fr]">
+	<div
+		class="mt-4 bg-white border border-gray-200 rounded-xl shadow-sm grid grid-cols-[auto,1fr]"
+	>
 		<!-- Sidebar -->
 		<SideNavbar class="p-6 h-screen fixed left-0 top-0" />
 		<!-- Router View -->
