af8e197943329f3d564dfdea7e638ce542f1fe65
S1lence-z <j3.zelenka@gmail.com>
2024-12-04 15:31:00 +0100

    chore: change app layout to grid

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import SideNavbar from './components/SideNavBar.vue';
 </script>
 
 <template>
-	<div class="w-full h-screen flex space-x-6 m-5">
+	<div class="mt-4 bg-white border border-gray-200 rounded-xl shadow-sm grid grid-cols-[auto,1fr]">
 		<!-- Sidebar -->
 		<SideNavbar class="p-6 h-screen fixed left-0 top-0" />
 		<!-- Router View -->
