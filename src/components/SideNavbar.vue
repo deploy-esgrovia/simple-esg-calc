cae12bbaa6214d6c93908416fa153ce2a88ea781
S1lence-z <j3.zelenka@gmail.com>
2024-12-04 12:26:54 +0100

    feat: style the side navbar

--- src/components/SideNavbar.vue
+++ src/components/SideNavbar.vue
@@ -1,55 +1,133 @@
-<template>
-	<div>
-		<nav>
-			<ul>
-				<!--Scope 1-->
-				<li>
-					<ul>
-						<li class="text-lg font-bold">
-							<p>Scope 1</p>
-						</li>
+<script setup>
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
 
-						<li>
-							<router-link to="/" class="block py-2 ">Energy</router-link>
+const route = useRoute()
+
+const isScope1Active = computed(() => {
+	return ['/energy', '/heat', '/heating-sources'].includes(route.path)
+})
+
+const isScope2Active = computed(() => {
+	return ['/vehicles', '/emissions'].includes(route.path)
+})
+
+const isScope3Active = computed(() => {
+	return ['/scope3'].includes(route.path)
+})
+</script>
+
+<template>
+	<div class="border border-x-1 border-y-0 border-l-0 border-gray-200 min-w-60">
+		<nav>
+			<ul class="text-lg">
+				<!-- Scope 1 -->
+				<li>
+					<ul
+						:class="{ 'border-l-4 border-0 border-opacity-50 border-blue-700': isScope1Active }"
+					>
+						<li
+							class="text-lg font-bold border bg-blue-500 bg-opacity-10 border-b-1 border-x-0 border-t-0"
+						>
+							<p class="block py-2 pl-3 pr-12">Scope 1</p>
 						</li>
-						<li>
-							<router-link to="/heat" class="block py-2 ">Heat</router-link>
-						</li>
-						<li>
-							<router-link to="/heating-sources" class="block py-2 "
-								>Heating Sources</router-link
+						<li
+							class="border border-gray-300 border-b-1 border-x-0 border-t-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/"
+								class="block py-2 pl-3 pr-3 justify-between transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50': $route.path === '/energy' }"
 							>
+								<div>Energy</div>
+							</RouterLink>
+						</li>
+						<li
+							class="border border-gray-300 border-b-1 border-x-0 border-t-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/heat"
+								class="block py-2 pl-3 pr-3 flex justify-between transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50 ': $route.path === '/heat' }"
+							>
+								<div>Heat</div>
+							</RouterLink>
+						</li>
+						<li
+							class="border border-gray-300 border-b-1 border-x-0 border-t-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/heating-sources"
+								class="block py-2 pl-3 pr-3 flex justify-between transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50 ': $route.path === '/heating-sources' }"
+							>
+								<div>Heating Sources</div>
+							</RouterLink>
 						</li>
 					</ul>
 				</li>
-				<!--Scope 2-->
+
+				<!-- Scope 2 -->
+				<li>
+					<ul
+						:class="{ 'border-l-4 border-0 border-opacity-50 border-blue-700': isScope2Active }"
+					>
+						<li
+							class="text-lg font-bold border bg-blue-500 bg-opacity-10 border-b-1 border-x-0 border-t-0"
+						>
+							<p class="block py-2 pl-3 pr-12">Scope 2</p>
+						</li>
+						<li
+							class="border border-gray-300 border-b-1 border-x-0 border-t-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/vehicles"
+								class="block py-2 pl-3 pr-3 flex justify-between transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50 ': $route.path === '/vehicles' }"
+							>
+								<div>Vehicles</div>
+							</RouterLink>
+						</li>
+						<li
+							class="border border-gray-300 border-b-1 border-x-0 border-t-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/emissions"
+								class="block py-2 pl-3 pr-3 flex justify-between transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50 ': $route.path === '/emissions' }"
+							>
+								<div>Emissions</div>
+							</RouterLink>
+						</li>
+					</ul>
+				</li>
+
+				<!-- Scope 3 -->
+				<li>
+					<ul
+						:class="{ 'border-l-4 border-0 border-opacity-50 border-blue-700': isScope3Active }"
+					>
+						<li
+							class="text-lg font-bold border bg-blue-500 bg-opacity-10 border-b-1 border-x-0 border-t-0"
+						>
+							<p class="block py-2 pl-3 pr-12">Scope 3</p>
+						</li>
+					</ul>
+				</li>
+
+				<!-- Summary -->
 				<li>
 					<ul>
-						<li class="text-lg font-bold">Scope 2</li>
-						<li>
-							<router-link to="/vehicles" class="block py-2 ">Vehicles</router-link>
-						</li>
-						<li>
-							<router-link to="/emissions" class="block py-2 ">Emissions</router-link>
-						</li>
-					</ul>
-				</li>
-				<!--Scope 3-->
-				<li>
-					<ul>
-						<li class="text-lg font-bold">
-							<p>Scope 3</p>
-						</li>
-					</ul>
-				</li>
-				<!--Závěr-->
-				<li class="pt-4">
-					<ul>
-						<li class="text-lg  font-bold">Shrnutí</li>
-						<li>
-							<router-link to="/company-info" class="block py-2 "
-								>Company Info</router-link
+						<li
+							class="font-bold border border-gray-300 border-t-4 border-b-1 border-x-0 last:border-b-0"
+						>
+							<RouterLink
+								to="/company-info"
+								class="block py-2 pl-3 pr-12 transition-colors duration-200"
+								:class="{ 'bg-blue-500 bg-opacity-50 ': $route.path === '/company-info' }"
 							>
+								Shrnutí
+							</RouterLink>
 						</li>
 					</ul>
 				</li>
