bd1925ffa2aa6c8277c47215fe533e06082b2707
S1lence-z <j3.zelenka@gmail.com>
2024-12-06 12:16:51 +0100

    feat: add api url env var and fetch the data on page load

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import SideNavbar from './components/SideNavbar.vue';
+import { useFormStore } from './stores/formStore';
+
+const formStore = useFormStore();
+formStore.fetchData();
 </script>
 
 <template>
-	<div class="m-4 bg-gray-100 border border-gray-200 rounded-xl shadow-sm grid grid-cols-[auto,1fr]">
+	<div
+		class="m-4 bg-gray-100 border border-gray-200 rounded-xl shadow-sm grid grid-cols-[auto,1fr]"
+	>
 		<!-- Sidebar -->
 		<SideNavbar class="border border-x-1 border-y-0 border-l-0 border-gray-200 min-w-60" />
 		<!-- Router View -->
--- src/stores/formStore.js
+++ src/stores/formStore.js
@@ -1,8 +1,12 @@
 import { defineStore } from "pinia";
 import { reactive } from "vue";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 // Define the Pinia store
 export const useFormStore = defineStore("formStore", () => {
+	const baseApiUrl = import.meta.env.VITE_API_URL;
+	const hash = reactive({ value: Cookies.get("hash") || null });
 	// Form data is now inside the store
 	const formData = reactive({
 		energy: {},
@@ -13,6 +17,34 @@ export const useFormStore = defineStore("formStore", () => {
 		companyInfo: {},
 	});
 
+	// Function for saving data
+	const saveData = async () => {
+		try {
+			if (hash.value) {
+				// Update existing data
+				await axios.post(`${baseApiUrl}?hash=${hash.value}`, formData);
+				return;
+			}
+			// Create new data
+			const response = await axios.post(baseApiUrl, formData);
+			hash.value = response.data.hash;
+			Cookies.set("hash", hash.value);
+		} catch (error) {
+			console.error("Error saving data: ", error);
+		}
+	};
+
+	// Function for fetching data
+	const fetchData = async () => {
+		if (!hash.value) return;
+		try {
+			const response = await axios.get(`${baseApiUrl}?hash=${hash.value}`);
+			Object.assign(formData, response.data);
+		} catch (error) {
+			console.error("Error fetching data: ", error);
+		}
+	};
+
 	// You can create actions or computed properties if needed
-	return { formData };
+	return { formData, saveData, fetchData };
 });
